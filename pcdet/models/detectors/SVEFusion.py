edb373afb9cec34a25111a3ad82943d1a7b875ab
17BuGs <147091304+17BuGs@users.noreply.github.com>
2025-04-13 23:58:20 +0800

    fix some code

--- pcdet/models/detectors/SVEFusion.py
+++ pcdet/models/detectors/SVEFusion.py
@@ -2,7 +2,7 @@ from .detector3d_template import Detector3DTemplate
 from ..backbones_3d import vfe
 
 
-class GSSMNet(Detector3DTemplate):
+class SVEFusion(Detector3DTemplate):
     def __init__(self, model_cfg, num_class, dataset):
         super().__init__(model_cfg=model_cfg, num_class=num_class, dataset=dataset)
         self.module_list = self.build_networks()
